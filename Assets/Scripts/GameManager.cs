8c19c77c3adde967a5e8fc43e6e4c1ff9cbe39a4
NCTP <cjfgh530@naver.com>
2025-05-08 14:21:09 +0900

    0508 Add GameManager and Starting Point
    - Add GameManager to track progress
    - Player spawns at starting position
    - Grip / Trigger action available

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public enum Progress
+    {
+        Tutorial, // Tutorial
+        First, // First puzzle, until clear
+        Second, // Second puzzle
+        Third, // Third puzzle
+        Last // End
+    }
+    public Transform startPoint;
+    public GameObject playerPrefab;
+    private Progress _progress = Progress.Tutorial;
+
+    // Change progress by calling this method.
+    public void SetProgress(Progress newProgress)
+    {
+        _progress = newProgress;
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameObject player = Instantiate(playerPrefab,startPoint.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        GUI.color = Color.red;
+        GUI.Label(new Rect(125, 40, 200, 20), "Progress : " + _progress);
+        if(GUILayout.Button("Tutorial"))
+        {
+            SetProgress(Progress.Tutorial);
+        }
+        if(GUILayout.Button("First"))
+        {
+            SetProgress(Progress.First);
+        }
+        if(GUILayout.Button("Second"))
+        {
+            SetProgress(Progress.Second);
+        }
+        if(GUILayout.Button("Third"))
+        {
+            SetProgress(Progress.Third);
+        }
+        if(GUILayout.Button("Last"))
+        {
+            SetProgress(Progress.Last);
+        }
+    }
+}
--- Assets/Scripts/PlayerHandController.cs
+++ Assets/Scripts/PlayerHandController.cs
@@ -8,21 +8,30 @@ public class PlayerHandController : MonoBehaviour
     public GameObject leftHand;
     public InputActionReference rightTriggerAction;
     public InputActionReference leftTriggerAction;
+    public InputActionReference rightGripAction;
+    public InputActionReference leftGripAction;
 
-    private float _rightTriggerInput, _leftTriggerInput;
+    private float _rightTriggerInput, _leftTriggerInput, _rightGripInput, _leftGripInput;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rightTriggerAction.action.Enable();
+        leftTriggerAction.action.Enable();
+        rightGripAction.action.Enable();
+        leftTriggerAction.action.Enable();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        /*
         _rightTriggerInput = rightTriggerAction.action.ReadValue<float>();
         _leftTriggerInput = leftTriggerAction.action.ReadValue<float>();
+        _rightGripInput = rightGripAction.action.ReadValue<float>();
+        _leftGripInput = leftGripAction.action.ReadValue<float>();
+        
         if (_rightTriggerInput >= 1)
         {
             Debug.Log("Hello World with Right Hand!");
@@ -31,6 +40,8 @@ public class PlayerHandController : MonoBehaviour
         {
             Debug.Log("Hello World with Left Hand!");
         }
+        */
+        
 
     }
     
